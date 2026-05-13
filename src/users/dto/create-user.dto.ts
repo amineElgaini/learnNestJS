c8253cddce560877abd1ad5557e99777612a3410
amineElgaini <elgainiamine@gmail.com>
2026-05-13 10:17:51 +0100

    initialze the app with simple get users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+
+}
